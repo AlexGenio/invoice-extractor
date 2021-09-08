40fe3e16d9b32516b89c59ffaf52abb8fe87f644
AlexGenio <alexgenio1995@gmail.com>
2021-09-07 22:31:39 -0400

    Improved parsing and post processing implementation to also process phone numbers and addresses of clients. Tests with 19 sample PDFs, needs to be tested more rigorously.

--- src/extractor_pdf.py
+++ src/extractor_pdf.py
@@ -15,12 +15,16 @@ class ExtractorPDF(Extractor,object):
                             root_dir,
                             output_dir)
 
-        self.__regex_client = re.compile(r'\b(^[A-ZÀÂÇÉÈÊËÎÏÔÛÙÜŸÑÆŒa-zàâçéèêëîïôûùüÿñæœ\s-]+)(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4})?\b')
-        self.__regex_email  = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
-        self.__doc_count    = 0
-        self.__filenames    = []
-        self.__clients      = []
-        self.__emails       = []
+        self.__regex_client  = re.compile(r'\b(^[A-ZÀÂÇÉÈÊËÎÏÔÛÙÜŸÑÆŒa-zàâçéèêëîïôûùüÿñæœ\s-]+)\b')
+        self.__regex_phone   = re.compile(r'\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}')
+        self.__regex_address = re.compile(r'\b(^\d+.*?\s+.+$)\b')
+        self.__regex_email   = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+        self.__doc_count     = 0
+        self.__filenames     = []
+        self.__clients       = []
+        self.__phone_numbers = []
+        self.__addresses     = []
+        self.__emails        = []
 
 
     def printHeader(self):
@@ -46,6 +50,7 @@ class ExtractorPDF(Extractor,object):
                     print("Processing file: {}".format(filename))
                     self.__doc_count += 1
 
+                    # parse the PDF
                     pdf  = fitz.open(filepath)
                     page = pdf[0]
                     text = page.get_text()
@@ -56,31 +61,86 @@ class ExtractorPDF(Extractor,object):
                     
                     client = ""
                     email  = ""
-                    client_next_line = False
+                    address = []
+                    phone_numbers = []
+
+                    process_sold = False
+                    process_deliver = False
+                    process_address = False
+                    lines_to_skip = 0
 
                     lines  = text.split('\n')
 
+                    # go through parsed PDF line per line
                     for line in lines:
                         line = line.strip()
 
-                        if not client:
-                            if "Sold To" in line or "Vendu à" in line or "Deliver To" in line or "Livrer à" in line:
-                                client_next_line = True
+                        # extract phone numbers, address, and email from "Sold To" section
+                        if "Sold To" in line or "Vendu à" in line:
+                            lines_to_skip = 1
+                            process_sold = True
+                            process_deliver = False
+                            continue
+
+                        if process_sold:
+
+                            result = self.__regex_phone.findall( line)
+                            for phone in result:
+                                if phone not in phone_numbers:
+                                    phone_numbers.append( phone)
+
+                            # force skip the client (could look like an address)
+                            if lines_to_skip > 0:
+                                lines_to_skip -= 1
                                 continue
 
-                            if client_next_line:
-                                result = self.__regex_client.search(line)
+                            if not email:
+                                result = self.__regex_email.search( line)
+                                if result:
+                                    email = result.group().strip().lower()
+                                else:
+                                    # build address string (after client, before email)
+                                    if process_address:
+                                        address.append( line.lower().title())
+                                    else:
+                                        result = self.__regex_address.search( line)
+                                        if result:
+                                            address.append( result.group(1).strip().lower().title())
+                                            process_address = True
+
+                            if email:
+                                process_sold = False
+                                continue
+
+                        # extract phone numbers, client name, and potentially address
+                        # from "Deliver To" section
+                        if "Deliver To" in line or "Livrer à" in line:
+                            process_sold = False
+                            process_deliver = True
+                            continue
+
+                        if process_deliver:
+
+                            result = self.__regex_phone.findall( line)
+                            for phone in result:
+                                if phone not in phone_numbers:
+                                    phone_numbers.append( phone)
+
+                            if not client:
+                                result = self.__regex_client.search( line)
                                 if result:
                                     client = result.group(1).strip().lower().title()
-                                client_next_line = False
+
+                            if not address:
+                                result = self.__regex_address.search( line)
+                                if result:
+                                    address.append( result.group(1).strip().lower().title())
+
+                            if client and address:
+                                process_deliver = False
                                 continue
 
-                        if not email:
-                            result = self.__regex_email.search(line)
-                            if result:
-                                email = result.group().strip().lower()
-
-                        if client and email:
+                        if client and address and email:
                             break
 
                     pdf.close()
@@ -91,17 +151,26 @@ class ExtractorPDF(Extractor,object):
                     if not client:
                         OUTPUT_LOGGER.log( logging.WARNING, "Failed to extract client full name.")
 
+                    if not phone_numbers:
+                        OUTPUT_LOGGER.log( logging.WARNING, "Failed to extract client phone number(s).")
+
+                    if not address:
+                        OUTPUT_LOGGER.log( logging.WARNING, "Failed to extract client address.")
+
                     if not email:
                         OUTPUT_LOGGER.log( logging.WARNING, "Failed to extract client email.")
                         valid_invoice = False
 
+                    # invoice should be processed if email is unique
                     if valid_invoice:
                         if email in self.__emails:
                             OUTPUT_LOGGER.log( logging.WARNING, "Duplicate email found --- Client: {}, Email: {}".format(client, email))
                         else:
                             self.__filenames.append( filename)
                             self.__clients.append( client)
+                            self.__addresses.append( address)
                             self.__emails.append( email)
+                            self.__phone_numbers.append( phone_numbers)
                             print("Extracted data --- Client: {}, Email: {}".format(client, email))
                     print("")
      
@@ -114,6 +183,8 @@ class ExtractorPDF(Extractor,object):
 
         print("*** {} PDF files extracted successfully ***".format(len(self.__filenames)))
         print("*** {} client full names extracted ***".format(len(self.__clients)))
+        print("*** {} client phone numbers extracted ***".format(len(self.__phone_numbers)))
+        print("*** {} client addresses extracted ***".format(len(self.__addresses)))
         print("*** {} client emails extracted ***".format(len( self.__emails)))
         print("")
 
@@ -122,14 +193,26 @@ class ExtractorPDF(Extractor,object):
             workbook = Workbook()
             worksheet = workbook.active
             worksheet.title = "Invoice Extraction"
-            headers = ["File", "First Name", "Last Name", "Email"]
+            headers = ["File", "First Name", "Last Name", "Phone Number(s)", "Address", "Email"]
             worksheet.append( headers)
 
+            # populate excel worksheet with parsed PDF data
             for idx in range(len(self.__filenames)):
-                firstname, *lastname = self.__clients[ idx].split()
-                lastname = " ".join(lastname)
-                worksheet.append( [self.__filenames[ idx], firstname, lastname, self.__emails[ idx]])
+                firstname = ""
+                lastname  = ""
+                if self.__clients[ idx]:
+                    firstname, *lastname = self.__clients[ idx].split()
+                    lastname             = " ".join(lastname)
+                phone_numbers        = ", ".join(self.__phone_numbers[ idx])
+                address              = ", ".join(self.__addresses[ idx])
+                worksheet.append( [self.__filenames[ idx],
+                                  firstname,
+                                  lastname,
+                                  phone_numbers,
+                                  address,
+                                  self.__emails[ idx]])
 
+            # automatically adjust column width
             for col in worksheet.columns:
                 max_length = 0
                 col[0].font = Font(bold=True, underline="single")
@@ -140,7 +223,7 @@ class ExtractorPDF(Extractor,object):
                             max_length = len(str(cell.value))
                     except:
                         pass
-                adjusted_width = (max_length + 2) * 1.2
+                adjusted_width = max_length * 1.05
                 worksheet.column_dimensions[column].width = adjusted_width
 
             workbook.save(self._output_file)
